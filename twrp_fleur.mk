e77f34acdcb9843f28818d973cbce6f1c55f29bc
ShazuxD <ShazuxD7@gmail.com>
2023-04-17 18:01:20 +0530

    fleur: twrp bringup
    Signed-off-by: ShazuxD <ShazuxD7@gmail.com>

---
+++ twrp_fleur.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from fleur device
+$(call inherit-product, device/xiaomi/fleur/device.mk)
+
+PRODUCT_DEVICE := fleur
+PRODUCT_NAME := twrp_fleur
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := fleur
+PRODUCT_MANUFACTURER := xiaomi
