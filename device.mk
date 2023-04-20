61ad6962c07ae1d27353511aa3a7a5bc69f96ca5
ShazuxD <ShazuxD7@gmail.com>
2023-04-20 18:42:06 +0530

    fleur: add libkeymaster41.so from 13.0.8.0.SKEMIXM
    Signed-off-by: ShazuxD <ShazuxD7@gmail.com>

--- device.mk
+++ device.mk
@@ -43,8 +43,10 @@ PRODUCT_COPY_FILES += \
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
+    libkeymaster41 \
     libpuresoftkeymasterdevice
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
