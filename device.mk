7ea62a13e3c82d981c16f3cc2ee462302d8ac253
ShazuxD <ShazuxD7@gmail.com>
2023-04-20 15:33:03 +0530

    fleur: some crypto flags and modules
    Signed-off-by: ShazuxD <ShazuxD7@gmail.com>

--- device.mk
+++ device.mk
@@ -34,3 +34,19 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+	android.hardware.gatekeeper@1.0-service
+
+PRODUCT_COPY_FILES += \
+	$(OUT_DIR)/target/product/fleur/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/android.hardware.gatekeeper@1.0-service
+
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
