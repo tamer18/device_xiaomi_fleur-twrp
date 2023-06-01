ecfc67a86f5f6b13df1227b9b6222f8114e59525
Your Name <you@example.com>
2023-06-01 11:26:46 +0530

    Drop Some Props

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -53,15 +53,6 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
 		export OF_AB_DEVICE=1
 		export OF_VIRTUAL_AB_DEVICE=1
 		export TARGET_DEVICE_ALT="miel"
-		
-		# OTA / DM-Verity / Encryption
-		export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-		export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-		export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
-		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-		export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
-		export OF_SKIP_FBE_DECRYPTION_SDKVERSION=34 # Don't try to decrypt A14(?)
-		export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
 
 		# Display / Leds
 		export OF_SCREEN_H="2400"
@@ -71,7 +62,6 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
 		export OF_HIDE_NOTCH=1
 		export OF_CLOCK_POS=1 # left and right clock positions available
 		export OF_USE_GREEN_LED=0
-	
 
 		# Other OrangeFox configs
 		export OF_ENABLE_LPTOOLS=1
