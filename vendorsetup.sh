6a528ec9ae148c12877f8a2ac03bb3b4aebb53f8
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-07-21 16:55:40 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -44,7 +44,6 @@ if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
 		# Version / Maintainer infos
 		export OF_MAINTAINER="Rohit_Tiwari"
-                export OF_MAINTAINER_AVATAR="$PWD/device/xiaomi/fleur/maintainer.png"
 		export FOX_VERSION=R12.1
 		export FOX_BUILD_TYPE="Stable"
                
